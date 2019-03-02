dd967010be2f3c5a2e584df3d4758b5bc4ed5208
viztopia <vinz.zhang@gmail.com>
2019-03-02 14:52:56 -0500

    sample api #1

---
+++ apiSrc/api sample 1.js
@@ -0,0 +1,38 @@
+let dcgan;
+let button, outputContainer, statusMsg;
+
+function setup() {
+    noCanvas();
+
+    //Load the model
+    dcgan = ml5.DCGAN('people', modelReady);
+
+    //status message
+    statusMsg = select('#status');
+
+    //button to generate an image
+    button = select('#generate');
+    button.mousePressed(generate);
+
+    //container for the output image
+    outputContainer = select('#output');
+}
+
+function generate() {
+    
+    dcgan.generate((err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        if (result && result.src) {
+            // Clear output container
+            outputContainer.html('');
+            // Create an image based result
+            createImg(result.src).parent('output');
+        }
+    });
+}
+
+function modelReady() {
+    select('#status').html('model ready');
+}
